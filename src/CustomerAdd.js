f3a059a27d3ef19764f8351d61d0c48a504a0081
hasuhasmath <hasuh5pepe@gmail.com>
2024-04-26 16:30:23 +0530

    end of customeradd

---
+++ src/CustomerAdd.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+export default function CustomerAdd() {
+  const [input, setInput] = useState("");
+  const [customers, setCustomers] = useState([]);
+  function addCustomer() {
+    if (input) {
+      setCustomers((previousState) => [...previousState, input]);
+    }
+  }
+  return (
+    <div> 
+      <h3>Add New Customer</h3>
+      <input type="text" onChange={(e) => setInput(e.target.value)} />
+      <button onClick={addCustomer}>Add</button>
+    </div>
+  );
+}
