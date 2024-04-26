186829d12396ac7dd5c2e02f7e55d2c2bd36942a
hasuhasmath <hasuh5pepe@gmail.com>
2024-04-27 00:29:41 +0530

    final

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import CustomerAdd from './CustomerAdd';
 import CustomerView from './CustomerView';
 import {Provider} from 'react-redux'
+import { store } from './store';
 
 function App() {
   return (
-    <Provider>
+    <Provider store={store}>
       <div className="App">
         <h1>React Redux Customer Add Example</h1>
         <CustomerAdd />
