5af4664cccd3207c69cafb5e3495f87ca0396c8f
hasuhasmath <hasuh5pepe@gmail.com>
2024-04-27 00:55:50 +0530

    devTools

--- src/store.js
+++ src/store.js
@@ -2,7 +2,8 @@ import {configureStore} from "@reduxjs/toolkit"
 import customerReducer from './slices/customerSlice'
 
 export const store = configureStore({
-    devTools:true,
+    // IF YOU DO NOT WANT TO HIDE THE DETAILS THEN MAKE THIS devTools into true
+    devTools:false,
     reducer: {
         customers: customerReducer,
   },
