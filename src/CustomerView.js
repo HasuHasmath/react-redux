6b0a92e3790a9b16ed44bd1043423a4f4b60be68
hasuhasmath <hasuh5pepe@gmail.com>
2024-04-27 00:37:09 +0530

    final

--- src/CustomerView.js
+++ src/CustomerView.js
@@ -3,12 +3,16 @@ import { useSelector } from 'react-redux';
 
 export default function CustomerView() {
 const customers =  useSelector((state)=> state.customers)
+
+function deleteHandler(index){
+
+}
   return (
     <div>
       <h2>Customer List</h2>
       <ul style={{listStyle: 'none',}}>
-        {customers.map((customer) => (
-          <li>{customer}</li>
+        {customers.map((customer, index) => (
+          <li>{customer} <button onClick={()=> deleteHandler}>delete</button></li>
         ))} 
       </ul>
     </div>
